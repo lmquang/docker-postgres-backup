83f260a0eed10530db727b8fc40b40617ec024db
Quang Le <quang@dwarvesv.com>
2019-03-11 13:39:46 +0700

    update postgres 10

--- run.sh
+++ run.sh
@@ -140,10 +140,10 @@ export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
 export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
 export AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION}
 
-echo $AWS_ACCESS_KEY_ID
-echo $AWS_SECRET_ACCESS_KEY
-echo $AWS_DEFAULT_REGION
-echo ${S3_BUCKET_URL}
+# echo $AWS_ACCESS_KEY_ID
+# echo $AWS_SECRET_ACCESS_KEY
+# echo $AWS_DEFAULT_REGION
+# echo ${S3_BUCKET_URL}
 
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
